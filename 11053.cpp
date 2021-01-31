e48750eff35288d680d83f311fc027d1bff2950d
bsu <bsu0541@naver.com>
2021-01-31 18:23:46 +0900

    clear 11053

--- 11053.cpp
+++ 11053.cpp
@@ -3,74 +3,33 @@
 
 using namespace std;
 
-int dp_incr(int idx);
-void dp_long(int idx);
-int min(int value, int idx);
-
-int N, max_cnt = 1;
+int N;
 int* arr;
-int dp[1000]; //idx: arr 값, dp[idx]: 최장 수열 길이
+int* dp;
 
 int main() {
 	cin >> N;
 
 	arr = new int[N];
-	for (int i = 0; i < N; i++)
+	dp = new int[N];
+	for (int i = 0; i < N; i++) {
 		cin >> arr[i];
+		dp[i] = 1;
+	}
 
-	for (int i = 0; i < N; i++)
-		dp_long(i);
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < i; j++) {
+			if ((arr[i] > arr[j]) && (dp[i] <= dp[j])) {
+				dp[i] = dp[j] + 1;
+			}
+		}
+	}
 
-	cout << max_cnt;
+	sort(dp, dp + N);
+
+	cout << dp[N - 1];
 
 	delete[] arr;
+	delete[] dp;
 	return 0;
-}
-
-int dp_incr(int idx) {
-	int index = 0;
-	if (index != -1)
-		return index = min(arr[idx], idx);
-	else
-		return 0;
-}
-
-int min(int v, int idx) {
-	int min = 1001, index = -1;
-	for (int i = idx + 1; i < N; i++) {
-		if ((arr[i] > v) && (arr[i] < min)) {
-			min = arr[i];
-			index = i;
-		}
-	}
-	return index;
-}
-
-void dp_long(int idx) {
-	int cnt = 1;
-	if (dp[arr[idx]] == 0) {
-		int num = arr[idx];
-		for (int i = idx + 1; i < N; i++) {
-			if (num < arr[i]) {
-				cnt++;
-				num = arr[i];
-			}
-		}
-		dp[arr[idx]] = cnt;
-	}
-	else {
-		if (N - idx > dp[arr[idx]]) { //남은 길이가 이미 저장된 값보다 큰 경우에만 실행
-			int num = arr[idx];
-			for (int i = idx; i < N; i++) {
-				if (num < arr[i]) {
-					cnt++;
-					num = arr[i];
-				}
-			}
-			if (dp[arr[idx]] < cnt)
-				cnt = dp[arr[idx]];
-		}
-	}
-	if (max_cnt < cnt)
-		max_cnt = cnt;
 }
