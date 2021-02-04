ae65487c7f2a0dea6875ac31f869d3d74075fd82
bsu <bsu0541@naver.com>
2021-02-04 20:47:35 +0900

    clear 9251

--- 9251.cpp
+++ 9251.cpp
@@ -1,49 +1,25 @@
 #include <iostream>
 #include <algorithm>
-//기저사례찾기 어려움, 반례 많이 필요 ...
+
 using namespace std;
 
-int N, idx = 0;
-int* dp;
+int N;
+int dp[1001][1001];
 
 int main() {
 	string str1, str2;
-	int n, n1, n2, start = 0, max = 0;
 
 	cin >> str1;
 	cin >> str2;
 
-	n1 = str1.size();
-	n2 = str2.size();
-
-	if (n1 > n2)
-		n = n1;
-	else
-		n = n2;
-
-	dp = new int[n + 1];
-	for (int i = 0; i < n + 1; i++)
-		dp[i] = 0;
-	
-	for (int i = 0; i < n1; i++) {
-		for (int j = start; j < n2; j++) {
-			if (str1[i] == str2[j]) {
-				if (j != n2 - 1) {
-					dp[idx + 1] = dp[idx] + 1;
-					start = j + 1;
-					if (max < dp[idx + 1])
-						max = dp[idx + 1];
-				}
-				idx++;
-				break;
-			}
+	for (int i = 0; i < str1.size(); i++) {
+ 		for (int j = 0; j < str2.size(); j++) {
+			if (str1[i] == str2[j])
+				dp[i + 1][j + 1] = dp[i][j] + 1;
+			else
+				dp[i + 1][j + 1] = max(dp[i][j + 1], dp[i + 1][j]);
 		}
 	}
-
-	/*for (int i = 0; i < n + 1; i++)
-		cout << dp[i] << " ";*/
-	cout << max;
-
-	delete[] dp;
+	cout << dp[str1.size()][str2.size()];
 	return 0;
 }
