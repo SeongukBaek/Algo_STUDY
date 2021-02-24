8782f86a68eca816005a5191ee6e4b85c8ffa44a
bsu <bsu0541@naver.com>
2021-02-24 17:10:39 +0900

    modified 10026

--- 10026.cpp
+++ 10026.cpp
@@ -8,7 +8,8 @@ int** visited;
 int x_ar[4] = { -1, 0, 0, 1 };
 int y_ar[4] = { 0, -1, 1, 0 };
 
-void color_cnt(int x, int y);
+void normal(int x, int y);
+void weak(int x, int y);
 
 int main() {
 	cin >> N;
@@ -27,15 +28,22 @@ int main() {
 	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < N; j++) {
 			if (visited[i][j] == -1) {
-				color_cnt(i, j);
-				if (picture[i][j] == 'R' || picture[i][j] == 'G')
-					weak_cnt++;
+				normal(i, j);
 				normal_cnt++;
 			}
 		}
 	}
 
-	cout << "ÀüÃ¼: " << normal_cnt << endl << "red+green" << redgreen << endl << "blue" << blue << endl << "weak_cnt" << weak_cnt;
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			if (visited[i][j] == 1) {
+				weak(i, j);
+				weak_cnt++;
+			}
+		}
+	}
+
+	cout << normal_cnt << " " << weak_cnt;
 	for (int i = 0; i < N; i++) {
 		delete[] picture[i];
 		delete[] visited[i];
@@ -45,7 +53,7 @@ int main() {
 	return 0;
 }
 
-void color_cnt(int x, int y) {
+void normal(int x, int y) {
 	if (visited[x][y] == -1) {
 		char color = picture[x][y];
 		visited[x][y] = 1;
@@ -54,12 +62,30 @@ void color_cnt(int x, int y) {
 			int new_x = x + x_ar[i], new_y = y + y_ar[i];
 
 			if (new_x >= 0 && new_x < N && new_y >= 0 && new_y < N) {
-				if (visited[new_x][new_y] == -1) {
-					if (color == 'B')
-						blue++;
-					else
-						redgreen++;
-					color_cnt(new_x, new_y);
+				if (visited[new_x][new_y] == -1 && picture[new_x][new_y] == color) {
+					normal(new_x, new_y);
+				}
+			}
+		}
+	}
+}
+
+void weak(int x, int y) {
+	if (visited[x][y] == 1) {
+		char color = picture[x][y];
+		visited[x][y] = -1;
+
+		for (int i = 0; i < 4; i++) {
+			int new_x = x + x_ar[i], new_y = y + y_ar[i];
+
+			if (new_x >= 0 && new_x < N && new_y >= 0 && new_y < N) {
+				if (visited[new_x][new_y] == 1) {
+					if (color == picture[new_x][new_y])
+						weak(new_x, new_y);
+					else if (color == 'R' && picture[new_x][new_y] == 'G')
+						weak(new_x, new_y);
+					else if (color == 'G' && picture[new_x][new_y] == 'R')
+						weak(new_x, new_y);
 				}
 			}
 		}
