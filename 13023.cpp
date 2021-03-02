7c53a19e168a2a2a9eced845d6af062f22878d79
bsu <bsu0541@naver.com>
2021-03-02 23:29:42 +0900

    Working on it ... 13023

--- 13023.cpp
+++ 13023.cpp
@@ -3,7 +3,7 @@
 #include <algorithm>
 using namespace std;
 
-int isExist(int idx);
+int isExist(int idx, int friends);
 
 bool comp(pair <int, int> a, pair <int, int> b) {
 	if (a.first == b.first)
@@ -11,43 +11,65 @@ bool comp(pair <int, int> a, pair <int, int> b) {
 	return a.first < b.first;
 }
 
-int N, M, friends = 1;
+int N, M;
 vector<pair <int, int>> f;
-int* visited;
+int visited[2000];
 
 int main() {
 	cin >> N >> M;
 	int m = M;
 
-	visited = new int[M];
 	while (m--) {
 		int a, b;
 		cin >> a >> b;
 		f.push_back(make_pair(a, b));
-		visited[m] = -1;
 	}
 	sort(f.begin(), f.end(), comp);
 
 	for (int i = 0; i < M; i++) {
-		if (isExist(i) == 1) {
+		int friends = 2;
+		for (int j = 0; j < N; j++)
+			visited[j] = -1;
+
+		if (isExist(i, friends) == 1) {
 			cout << "1";
 			return 0;
 		}
-		friends = 1;
 	}
 	cout << "0";
 	return 0;
 }
 
-int isExist(int idx) {
-	if (friends == M)
+int isExist(int idx, int friends) {
+	if (friends == 5)
 		return 1;
 	else {
-		for (int i = idx + 1; i < M; i++) {
-			if (f[i].first == f[idx].first || f[i].second == f[idx].first || f[i].first == f[idx].second || f[i].second == f[idx].second) {
-				friends++;
-				return isExist(i);
+		int a = f[idx].first, b = f[idx].second, check = friends;
+		if (visited[a] == -1) {
+			visited[a] = 1;
+			for (int i = 0; i < M; i++) {
+				if (i != idx) {
+					if (f[i].first == a || f[i].second == a) {
+						friends++;
+						return isExist(i, friends);
+					}
+				}
 			}
+			if (check == friends)
+				friends--;
+		}
+		if (visited[b] == -1) {
+			visited[b] = 1;
+			for (int i = 0; i < M; i++) {
+				if (i != idx) {
+					if (f[i].first == b || f[i].second == b) {
+						friends++;
+						return isExist(i, friends);
+					}
+				}
+			}
+			if (check == friends)
+				friends--;
 		}
 	}
 }
