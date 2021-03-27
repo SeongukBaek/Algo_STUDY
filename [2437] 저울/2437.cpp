62508b2289c0af6432e56ec7b6f28f36b690debb
bsu <bsu0541@naver.com>
2021-03-28 00:44:27 +0900

    clear 2437

--- [2437] 저울/2437.cpp
+++ [2437] 저울/2437.cpp
@@ -1,36 +1,25 @@
 #include <iostream>
 #include <vector>
-#include <string>
 #include <algorithm>
 
 using namespace std;
 
-int main()
-{
-	int size, n, ans = 0, sum = 0;
-	cin >> size;
+int main() {
+	vector<int> w;
+	int N, n, ans = 0, sum = 0;
+	cin >> N;
 
-	vector<int> num(size);
-
-	for (int i = 0; i < size; i++) {
+	for (int i = 0; i < N; i++) {
 		cin >> n;
-		num[i] = n;
+		w.push_back(n);
 	}
-	sort(num.begin(), num.end());
+	sort(w.begin(), w.end());
 
-	for (n = 0; n < size - 1; n++) {
-		if (num[0] != 1)
+	for (int i = 0; i < N; i++) {
+		if (sum + 1 < w[i])
 			break;
-		else if (num[n + 1] - num[n] > 1) {
-			ans = num[n];
-			break;
-		}
+		sum += w[i];
 	}
-
-	ans++;
-	while (1) { // 1부터 올라가면서 주어진 추로 측정가능한지 확인, 불가능하면 출력
-		int sum = 0;
-		
-	}
-	cout << ans;
+	cout << sum + 1;
+	return 0;
 }
