d45d39d1f4b5355c2546312d0b1c28d144d45b1c
bsu <bsu0541@naver.com>
2021-04-02 01:53:29 +0900

    clear 1753.cpp + readme

--- [1753] 최단경로/1753.cpp
+++ [1753] 최단경로/1753.cpp
@@ -1,22 +1,41 @@
 #include <iostream>
 #include <vector>
 #include <queue>
-#include <cmath>
+#define INF 987654321
 using namespace std;
-
-void sp();
-int short_dist(int goal);
 
 int v, e, s;
 vector <pair <int, int>>graph[20001];
-int* cost;
+
+vector<int> short_dist() {
+	vector<int> cost(v + 1, INF);
+	cost[s] = 0;
+	priority_queue<pair <int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+	pq.push(make_pair(0, s));
+
+	while (!pq.empty()) {
+		int cur_cost = pq.top().first;
+		int cur = pq.top().second;
+		pq.pop();
+
+		if (cost[cur] < cur_cost) continue;
+		for (int i = 0; i < graph[cur].size(); i++) {
+			int next = graph[cur][i].first;
+			int next_cost = cur_cost + graph[cur][i].second;
+
+			if (cost[next] > next_cost) {
+				//현재 next에 저장된 cost의 값보다 현재의 점을 거쳐서 갈 경우의 거리가
+				//더 짧으면 갱신, 큐에 push
+				cost[next] = next_cost;
+				pq.push(make_pair(next_cost, next));
+			}
+		}
+	}
+	return cost;
+}
 
 int main() {
 	cin >> v >> e >> s;
-
-	cost = new int[v + 1];
-	fill_n(cost, v + 1, 11);
-	cost[s] = 0;
 
 	for (int i = 0; i < e; i++) {
 		int a, b, c;
@@ -24,41 +43,14 @@ int main() {
 		graph[a].push_back(make_pair(b, c));
 	}
 
-	sp();
+	vector <int> ans = short_dist();
 
-	for (int i = 1; i < v + 1; i++) {
-		if (cost[i] == 11)
+	for (int i = 1; i < ans.size(); i++) {
+		if (ans[i] == INF)
 			cout << "INF" << "\n";
 		else
-			cout << cost[i] << "\n";
+			cout << ans[i] << "\n";
 	}
 
 	return 0;
-}
-
-void sp() {
-	for (int i = 1; i < v + 1; i++) {
-		if (s == i)
-			cost[i] = 0;
-		else {
-			cost[i] = short_dist(i);
-		}
-	}
-}
-
-int short_dist(int goal) {
-	int w = cost[goal];
-
-	queue <int> q;
-	for (int k = 1; k < v + 1; k++) {
-		q.push(k);
-		for (int i = k; i < v + 1; i++) {
-			if (w > graph[s][k] + graph[k][goal])
-				w = graph[s][k] + graph[k][goal];
-		}
-		
-	}
-		
-	
-	return w;
 }
