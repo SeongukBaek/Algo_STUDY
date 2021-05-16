1cbc2fccf326551fcde70c518bc6dcabee54c4e1
bsu <bsu0541@naver.com>
2021-05-16 21:33:16 +0900

    Update 2887.cpp + readme

--- 1712.cpp
+++ 1712.cpp
@@ -4,25 +4,13 @@ using namespace std;
 
 int main() {
 	int a, b, c;
-	int year = 0;
-	int this_benefit = 0;
 
 	cin >> a >> b >> c;
 
-	while (1) {
-		this_benefit = c * year - (a + b * year);
-		if (year == 2) {
-			int last_benefit = c * (year - 1) - (a + b * (year - 1));
-			if (this_benefit - last_benefit < 0) {
-				cout << "-1";
-				break;
-			}
-		}
-		if (this_benefit > 0) {
-			cout << year;
-			break;
-		}
-		year++;
-	}
+	if (b >= c)
+		cout << -1;
+	else
+		cout << (a / (c - b)) + 1;
+	
 	return 0;
 }
